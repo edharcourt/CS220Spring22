3e4d348a5991f935e93f095ceed5bdc9f550c19a
Ed Harcourt <edharcourt@stlawu.edu>
2022-04-25 10:21:28 -0400

    .

--- bigint/bigint.h
+++ bigint/bigint.h
@@ -7,8 +7,8 @@ typedef struct {
 } BigInt;
 
 
-// make_int allocates memory for the big integer
-// it is the job of the caller to free it when 
+// make_int allocates memory for the big integer.
+// It is the job of the caller to free it when 
 // done using it.
 extern BigInt * make_int(const char *s);
 
