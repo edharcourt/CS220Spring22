5df8cac10698b179314dc9906364dca736502765
Ed Harcourt <edharcourt@stlawu.edu>
2022-05-06 08:02:12 -0400

    .

--- bigint/bigint2bin.c
+++ bigint/bigint2bin.c
@@ -1,5 +1,5 @@
-from typing import Tuple 
 
+/*
 def div2(s: str) -> Tuple[str, int, bool]:
     
     q = ""                      # quotient
@@ -22,8 +22,31 @@ def div2(s: str) -> Tuple[str, int, bool]:
     r = r - 2*p    # compute final 
 
     return (q,r,z)
+*/
 
+typedef byte unsigned char;
 
+byte div2(char *s, char *q, int *z) {
+
+    int r = s[0] - '0';
+    *z = 1;
+    int p = 0;
+
+    for (int i = 0; i < strlen(s); i++) {
+        p = r/2; 
+        if (p != 0)
+            *z = 0; 
+
+        *q++ = p + '0';
+
+        if (i < strlen(s) - 1)
+            r = (r - 2*p)*10 + (s[i+1] - '0')
+    }
+    r = r - 2*p;
+    *q = 0;  // null terminate q
+} 
+
+/* 
 def dec2bin(q: str) -> str:
 
     digs = ""    # result binary string
@@ -37,12 +60,18 @@ def dec2bin(q: str) -> str:
             break
 
     return '0b' + digs
+*/
 
+char *dec2bin(char *s) {
+    char *digs = malloc(4*strlen(s) + 1);
+    char q[strlen(s) + 1];
+    int z = 0;
 
-# main
-import sys
-b = dec2bin(sys.argv[1])
-print(b)
+    while (1) {
+        byte r = div2(s, q, &z);
+        
+    }
 
-print(bin(int(sys.argv[1])) == b) 
+}
+
 
